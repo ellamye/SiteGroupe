d7811e411c2f22ec1359ac79fa6fc316720b80f0
Massya KONE <massya.kone@gmail.com>
2020-04-06 15:04:43 +0200

    modification navbar et contenu

--- index.php
+++ index.php
@@ -21,6 +21,12 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">A propos</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">CGV</a>
+                </li>
                 </ul>
             </div>
         </nav>
@@ -28,6 +34,16 @@
         <section style="min-height: 90vh;">
 
         <h1 class="col-md-8 mx-auto display-4 text-center alert alert-info mt-3">Git hub</h1>
+
+
+        <h3 class="col-md-8 mx-auto display-4 text-center alert alert-success">Add/Push/Pull/</h3>
+
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis 
+            quibusdam delectus inventore sed atque harum ex, ea aut accusantium quas 
+            repudiandae quos deserunt assumenda non asperiores unde labore voluptatibus rem.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis 
+            quibusdam delectus inventore sed atque harum ex, ea aut accusantium quas 
+            repudiandae quos deserunt assumenda non asperiores unde labore voluptatibus rem.</p>
         
         </section>
 
